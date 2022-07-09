50f16643db5830327532e89b79cce450cd5d8bfe
Lukasz Zatyka <lukasz.zatyka@fluxninja.com>
2022-07-09 11:39:17 +0200

    Selector for useStaff

--- client/src/components/staff/hooks/useStaff.ts
+++ client/src/components/staff/hooks/useStaff.ts
@@ -1,9 +1,10 @@
-import { Dispatch, SetStateAction, useState } from 'react';
+import { Dispatch, SetStateAction, useCallback, useState } from 'react';
 
 import type { Staff } from '../../../../../shared/types';
 import { axiosInstance } from '../../../axiosInstance';
 import { queryKeys } from '../../../react-query/constants';
 import { useQuery } from 'react-query';
+import { filterByTreatment } from '../utils';
 
 // for when we need a query function for useQuery
 async function getStaff(): Promise<Staff[]> {
@@ -20,7 +21,14 @@ interface UseStaff {
 export function useStaff(): UseStaff {
   const [filter, setFilter] = useState('all');
 
-  const { data: staff = [] } = useQuery(queryKeys.staff, getStaff);
+  const filterFn = useCallback(
+    (unfilteredStaff) => filterByTreatment(unfilteredStaff, filter),
+    [filter],
+  );
+
+  const { data: staff = [] } = useQuery(queryKeys.staff, getStaff, {
+    select: filter !== 'all' ? filterFn : undefined,
+  });
 
   return { staff, filter, setFilter };
 }
