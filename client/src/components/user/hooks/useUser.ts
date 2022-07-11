aa6fdde824d7f4978a740ad46f00cd707fd9b8ad
Lukasz Zatyka <lukasz.zatyka@fluxninja.com>
2022-07-11 21:37:10 +0200

    call useQuery to update user data from server

--- client/src/components/user/hooks/useUser.ts
+++ client/src/components/user/hooks/useUser.ts
@@ -8,17 +8,18 @@ import {
   getStoredUser,
   setStoredUser,
 } from '../../../user-storage';
+import { useQuery, useQueryClient } from 'react-query';
 
-// async function getUser(user: User | null): Promise<User | null> {
-//   if (!user) return null;
-//   const { data }: AxiosResponse<{ user: User }> = await axiosInstance.get(
-//     `/user/${user.id}`,
-//     {
-//       headers: getJWTHeader(user),
-//     },
-//   );
-//   return data.user;
-// }
+async function getUser(user: User | null): Promise<User | null> {
+  if (!user) return null;
+  const { data }: AxiosResponse<{ user: User }> = await axiosInstance.get(
+    `/user/${user.id}`,
+    {
+      headers: getJWTHeader(user),
+    },
+  );
+  return data.user;
+}
 
 interface UseUser {
   user: User | null;
@@ -27,8 +28,7 @@ interface UseUser {
 }
 
 export function useUser(): UseUser {
-  // TODO: call useQuery to update user data from server
-  const user = null;
+  const { data: user } = useQuery(queryKeys.user, () => getUser(user));
 
   // meant to be called from useAuth
   function updateUser(newUser: User): void {
