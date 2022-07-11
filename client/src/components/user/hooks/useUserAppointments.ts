150f7c5bd2669ea898e78b22f351bebfc9fa1252
Lukasz Zatyka <lukasz.zatyka@fluxninja.com>
2022-07-11 22:17:16 +0200

    Dependent queries in userAppointments

--- client/src/components/user/hooks/useUserAppointments.ts
+++ client/src/components/user/hooks/useUserAppointments.ts
@@ -4,19 +4,28 @@ import type { Appointment, User } from '../../../../../shared/types';
 import { axiosInstance, getJWTHeader } from '../../../axiosInstance';
 import { queryKeys } from '../../../react-query/constants';
 import { useUser } from './useUser';
+import { useQuery } from 'react-query';
 
 // for when we need a query function for useQuery
-// async function getUserAppointments(
-//   user: User | null,
-// ): Promise<Appointment[] | null> {
-//   if (!user) return null;
-//   const { data } = await axiosInstance.get(`/user/${user.id}/appointments`, {
-//     headers: getJWTHeader(user),
-//   });
-//   return data.appointments;
-// }
+async function getUserAppointments(
+  user: User | null,
+): Promise<Appointment[] | null> {
+  if (!user) return null;
+  const { data } = await axiosInstance.get(`/user/${user.id}/appointments`, {
+    headers: getJWTHeader(user),
+  });
+  return data.appointments;
+}
 
 export function useUserAppointments(): Appointment[] {
   // TODO replace with React Query
-  return [];
+  const { user } = useUser();
+  const { data: userAppointments = <Appointment[]>[] } = useQuery(
+    'user-appointment',
+    () => getUserAppointments(user),
+    {
+      enabled: !!user,
+    },
+  );
+  return userAppointments;
 }
